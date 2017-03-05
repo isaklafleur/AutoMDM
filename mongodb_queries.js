bb7f95a20f2fb40f9336176e3c1683022bc5a6e2
Isak La Fleur <isak.engdahl@gmail.com>
2017-03-05 14:50:57 +0100

    Initial commit

---
+++ mongodb_queries.js
@@ -0,0 +1,2 @@
+db.getCollection('eclasses').find({ hierarchical_position: { $regex: /13/ } }).sort({ hierarchical_position: 1 })
+db.getCollection('eclasses').find({ its_superclass: "0173-1#01-AFX974#001" }).sort({ hierarchical_position: 1 })
