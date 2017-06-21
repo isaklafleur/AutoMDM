0a56397f14f2d0b8861759c7434fc2eb976373d8
Darek Glowinski <dariushg32111@gmail.com>
2017-06-21 18:41:07 +0200

    fuzzy search

--- models/eclass.js
+++ models/eclass.js
@@ -8,12 +8,48 @@ const eClassSchema = new mongoose.Schema({
   eclassMainGroup: { type: String, min: 2, max: 2 },
   eclassGroup: { type: String, min: 2, max: 2 },
   eclassCommodityClass: { type: String, min: 2, max: 2 },
-  preferredName: { type: String, max: 80 },
-  definition: { type: String, max: 1023 },
-  level: { type: String, min: 1, max: 1 },
-  mkSubclass: { type: String, min: 1, max: 1 },
-  mkKeyword: { type: String, min: 1, max: 1 },
-});
 
+  supplier: String,
+  idCC:String,
+  identifier:String,
+  versionNumber: String,
+  versionDate: String,
+  revisionNumber: String,
+  codedName: String,
+  preferredName: String,
+  definition: String,
+  ISOLanguage: String,
+  ISOCountryCode: String,
+  note: String,
+  remark: String,
+  level: String,
+  mkSubclass: String,
+  mkKeyword: String,
+  irdiCC: String,
+
+  keyword: String,
+  // preferredName:{ type: String, max: 80 },
+  // definition: { type: String, max: 1023 },
+  // level: { type: String, min: 1, max: 1 },
+  // mkSubclass: { type: String, min: 1, max: 1 },
+  // mkKeyword: { type: String, min: 1, max: 1 },
+});
+// 0 Supplier;
+// 1 IdCC;
+// 2 Identifier;
+// 3 VersionNumber;
+// 4 VersionDate;
+// 5 RevisionNumber;
+// 6 CodedName;
+// 7 PreferredName;
+// 8 Definition;
+// 9 ISOLanguageCode;
+// 10 ISOCountryCode;
+// 11 Note;
+// 12 Remark;
+// 13 Level;
+// 14 MKSubclass;
+// 15 MKKeyword;
+// 16 IrdiCC
 // Create mongoose model
-module.exports = mongoose.model('EClass', eClassSchema);
+module.exports = mongoose.model('EClass', eClassSchema);;
---
+++ models/unspsc.js
@@ -0,0 +1,24 @@
+
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+// Defining Mongoose Schema
+const unspscSchema = new Schema({
+
+  segment: String,
+  segmentTitle: String,
+  family: String,
+  familyTitle: String,
+  class: String,
+  classTitle: String,
+  key: String,
+  commodity: String,
+  commodityTitle: String,
+  definition: String,
+  synonym: String
+
+
+});
+
+// Create mongoose model
+module.exports = mongoose.model('Unspsc', unspscSchema);
