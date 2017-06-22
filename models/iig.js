bce680a4f145b1d0829a54e6339acb1f75f1cd69
Isak La Fleur <isak.engdahl@gmail.com>
2017-06-22 14:02:41 +0200

    added iig file import

---
+++ models/iig.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = require('bluebird');
+
+// Defining Mongoose Schema
+const iigSchema = new mongoose.Schema({
+  name: String,
+  code: String,
+  fiig: String,
+  description: String,
+  effectiveDate: String,
+  colloquialItems: Array,
+  fscs: Array,
+  changelog: String,
+});
+
+// Create mongoose model
+module.exports = mongoose.model('IIG', iigSchema);
