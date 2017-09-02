bf610a4450db1dd9caf5d5a41de9422b7adb85c4
Isak La Fleur <isak.engdahl@gmail.com>
2017-09-02 17:12:38 +0200

    moved old import files

--- server/operations/database/partImport.js
+++ server/operations/database/partImport.js
@@ -1,8 +1,10 @@
-const fs = require("fs");
 const mongoose = require("mongoose");
-mongoose.Promise = global.Promise;
-const ACpart = require("../../models/acparts");
+const parse = require("csv-parse");
+const path = require("path");
+const fs = require("fs");
+const CompanyPart = require("../../models/parts");
 require("dotenv").config({ path: "../../.env" });
+mongoose.Promise = global.Promise;
 
 mongoose.connect(process.env.MONGODB_URI, {
   keepAlive: true,
@@ -10,28 +12,32 @@ mongoose.connect(process.env.MONGODB_URI, {
   useMongoClient: true
 });
 
-const lineReader = require("readline").createInterface({
-  input: fs.createReadStream("./../../data/parts.csv")
+const p = path.join(__dirname, "/../../", "data");
+// console.log(p);
+const parserParts = parse({ delimiter: ";" }, (err, data) => {
+  //   console.log(data[1][0]);
+
+  // Looping and storing the data into mongodb
+  console.log("data.length: ", data.length);
+  for (let i = 1; i < data.length; i++) {
+    if (data[i][0] === "SDC") {
+      const newPart = new CompanyPart();
+      newPart.facility = data[i][0];
+      newPart.itemNumber = data[i][1];
+      newPart.partName = data[i][2];
+      newPart.partDescription = data[i][3];
+      newPart.netWeight = data[i][4];
+      newPart.customsTariff = data[i][5];
+      newPart
+        .save()
+        .then(() => {
+          mongoose.disconnect();
+        })
+        .catch(err => {
+          console.log("There was an error", err);
+        });
+    }
+  }
 });
 
-lineReader.on("line", line => {
-  const l = line.split(";");
-  const r = {};
-
-  r.facility = l[0];
-  r.itemNumber = l[1];
-  r.partName = l[2];
-  r.partDescription = l[3];
-  r.netWeight = l[4];
-  r.customsTariff = l[5];
-  const acparts = new ACpart(r);
-
-  acparts
-    .save()
-    .then(() => {
-      mongoose.disconnect();
-    })
-    .catch(err => {
-      console.log("There was an error", err);
-    });
-});
+fs.createReadStream(`${p}/parts.csv`).pipe(parserParts);
